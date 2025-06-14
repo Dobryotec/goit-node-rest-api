2520b9bd02a11674e6fa085f8df75666489c6f39
Dobryotec <dobrobatka@gmail.com>
2025-06-14 19:27:48 +0300

    add routes, schemas,controllers and services for rest api

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,60 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from '../helpers/HttpError.js';
+import {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+  modifyContact,
+} from '../services/contactsServices.js';
+import { ctrlWrapper } from '../helpers/ctrlWrapper.js';
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const result = await listContacts();
 
-export const getOneContact = (req, res) => {};
+  return res.json(result);
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await getContactById(id);
 
-export const createContact = (req, res) => {};
+  if (!result) throw HttpError(404);
 
-export const updateContact = (req, res) => {};
+  return res.json(result);
+};
+
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await removeContact(id);
+
+  if (!result) throw HttpError(404);
+
+  return res.json(result);
+};
+
+export const createContact = async (req, res) => {
+  const result = await addContact(req.body);
+
+  return res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+
+  if (Object.keys(req.body).length === 0)
+    throw HttpError(400, 'Body must have at least one field');
+
+  const result = await modifyContact(id, req.body);
+
+  if (!result) throw HttpError(404);
+
+  return res.json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+};
